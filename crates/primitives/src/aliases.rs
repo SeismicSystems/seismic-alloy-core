9b76372fdfc71998c8e7f64a2c36266baed2447c
Christian Drappi <christiandrappi@gmail.com>
2024-09-18 15:10:09 -0400

    make suint/sint primitive types store uint/int

--- crates/primitives/src/aliases.rs
+++ crates/primitives/src/aliases.rs
@@ -75,7 +75,7 @@ int_aliases! {
     Hash,
     Debug,
 )]
-pub struct SUInt<const BITS: usize>(pub U256);
+pub struct SUInt<const BITS: usize, const LIMBS: usize>(pub Uint<BITS, LIMBS>);
 
 #[cfg(feature = "seismic")]
 #[doc = "seismic unsigned integer type][Suint], where the preimage is an unsigned integer"]
@@ -90,16 +90,16 @@ pub struct SUInt<const BITS: usize>(pub U256);
     Hash,
     Debug,
 )]
-pub struct SInt<const BITS: usize>(pub U256);
+pub struct SInt<const BITS: usize, const LIMBS: usize>(pub Signed<BITS, LIMBS>);
 
 #[cfg(feature = "seismic")]
 macro_rules! sint_aliases {
-    ($($unsigned:ident, $signed:ident<$BITS:literal>),* $(,)?) => {$(
+    ($($unsigned:ident, $signed:ident<$BITS:literal, $LIMBS:literal>),* $(,)?) => {$(
         #[doc = concat!($BITS, "-bit [seismic unsigned integer type][Suint], where the preimage is an unsigned integer with ", $BITS, " bits.")]
-        pub type $unsigned = SUInt<$BITS>;
+        pub type $unsigned = SUInt<$BITS, $LIMBS>;
 
         #[doc = concat!($BITS, "-bit [seismic signed integer type][Sint], where the preimage is a signed integer with ", $BITS, " bits.")]
-        pub type $signed = SInt<$BITS>;
+        pub type $signed = SInt<$BITS, $LIMBS>;
     )*};
 }
 
@@ -117,58 +117,58 @@ impl arbitrary::Arbitrary<'_> for SAddress {
 }
 
 #[cfg(all(feature = "seismic", feature = "arbitrary"))]
-impl<const BITS: usize> arbitrary::Arbitrary<'_> for SUInt<BITS> {
+impl<const BITS: usize, const LIMBS: usize> arbitrary::Arbitrary<'_> for SUInt<BITS, LIMBS> {
     fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
-        let arbitrary_u256 = u.arbitrary::<U256>()?;
-        Ok(SUInt(arbitrary_u256))
+        let arbitrary_uint = u.arbitrary::<Uint<BITS, LIMBS>>()?;
+        Ok(SUInt(arbitrary_uint))
     }
 }
 
 #[cfg(all(feature = "seismic", feature = "arbitrary"))]
-impl<const BITS: usize> arbitrary::Arbitrary<'_> for SInt<BITS> {
+impl<const BITS: usize, const LIMBS: usize> arbitrary::Arbitrary<'_> for SInt<BITS, LIMBS> {
     fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
-        let arbitrary_u256 = u.arbitrary::<U256>()?;
-        Ok(SInt(arbitrary_u256))
+        let arbitrary_signed = u.arbitrary::<Signed<BITS, LIMBS>>()?;
+        Ok(SInt(arbitrary_signed))
     }
 }
 
 #[cfg(feature = "seismic")]
 sint_aliases! {
-   SU8,   SI8<  8 >,
-   SU16,  SI16< 16>,
-   SU24,  SI24< 24>,
-   SU32,  SI32< 32>,
-   SU40,  SI40< 40>,
-   SU48,  SI48< 48>,
-   SU56,  SI56< 56>,
-   SU64,  SI64< 64>,
+   SU8,   SI8<  8, 1>,
+   SU16,  SI16< 16, 1>,
+   SU24,  SI24< 24, 1>,
+   SU32,  SI32< 32, 1>,
+   SU40,  SI40< 40, 1>,
+   SU48,  SI48< 48, 1>,
+   SU56,  SI56< 56, 1>,
+   SU64,  SI64< 64, 1>,
 
-   SU72,  SI72< 72>,
-   SU80,  SI80< 80>,
-   SU88,  SI88< 88>,
-   SU96,  SI96< 96>,
-   SU104, SI104<104>,
-   SU112, SI112<112>,
-   SU120, SI120<120>,
-   SU128, SI128<128>,
+   SU72,  SI72< 72, 2>,
+   SU80,  SI80< 80, 2>,
+   SU88,  SI88< 88, 2>,
+   SU96,  SI96< 96, 2>,
+   SU104, SI104<104, 2>,
+   SU112, SI112<112, 2>,
+   SU120, SI120<120, 2>,
+   SU128, SI128<128, 2>,
 
-   SU136, SI136<136>,
-   SU144, SI144<144>,
-   SU152, SI152<152>,
-   SU160, SI160<160>,
-   SU168, SI168<168>,
-   SU176, SI176<176>,
-   SU184, SI184<184>,
-   SU192, SI192<192>,
+   SU136, SI136<136, 3>,
+   SU144, SI144<144, 3  >,
+   SU152, SI152<152, 3>,
+   SU160, SI160<160, 3>,
+   SU168, SI168<168, 3>,
+   SU176, SI176<176, 3>,
+   SU184, SI184<184, 3>,
+   SU192, SI192<192, 3>,
 
-   SU200, SI200<200>,
-   SU208, SI208<208>,
-   SU216, SI216<216>,
-   SU224, SI224<224>,
-   SU232, SI232<232>,
-   SU240, SI240<240>,
-   SU248, SI248<248>,
-   SU256, SI256<256>,
+   SU200, SI200<200, 4>,
+   SU208, SI208<208, 4>,
+   SU216, SI216<216, 4>,
+   SU224, SI224<224, 4>,
+   SU232, SI232<232, 4>,
+   SU240, SI240<240, 4>,
+   SU248, SI248<248, 4>,
+   SU256, SI256<256, 4>,
 }
 
 macro_rules! fixed_bytes_aliases {
