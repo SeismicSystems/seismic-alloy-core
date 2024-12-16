5c955da5a187ac532fe974542b7cef759eb69253
Christian Drappi <c@seismic.systems>
2024-12-16 16:33:10 -0500

    seismic_transaction: remove seismic_util lib (#22)

--- crates/seismic-transaction/src/lib.rs
+++ crates/seismic-transaction/src/lib.rs
@@ -6,8 +6,5 @@ pub mod encoding_decoding;
 /// Module for transaction-related functionalities.
 pub mod transaction;
 
-/// Module for utility functions.
-pub mod seismic_util;
-
 /// Module for type definitions and related functionalities.
 pub mod types;
