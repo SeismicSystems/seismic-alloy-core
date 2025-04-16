db7f1aa78ee555529817f0456b2531f1c67d640c
phexyz <32248504+phexyz@users.noreply.github.com>
2025-04-16 15:33:20 -0400

    last commit

--- crates/sol-types/src/types/value.rs
+++ crates/sol-types/src/types/value.rs
@@ -2,7 +2,7 @@ use super::SolType;
 use crate::{
     abi::TokenSeq,
     private::SolTypeValue,
-    sol_data::{self, ByteCount, Sbool, SupportedFixedBytes},
+    sol_data::{self, ByteCount, SupportedFixedBytes},
     Result, Word,
 };
 use alloc::{borrow::Cow, string::String, vec::Vec};
@@ -270,7 +270,7 @@ impl_sol_value! {
 
 #[cfg(feature = "seismic")]
 impl_sol_value! {
-    [] Sbool => sol_data::Sbool [];
+    [] crate::types::data_type::Sbool => sol_data::Sbool [];
     [] SAddress => sol_data::Saddress [];
     []     SU8 => sol_data::Suint<8> [];
     []     SU16 => sol_data::Suint<16> [];
